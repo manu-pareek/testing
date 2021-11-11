c0239b679e4dac479406d927fe0b2dfa7525955a
Manu Pareek <74110265+manu-pareek@users.noreply.github.com>
2021-11-11 13:06:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Foreground from "./Components/Foreground.js";
+
+console.log("inside index-foreground");
+
+ReactDOM.render(<Foreground />, document.getElementById("foreground"));
