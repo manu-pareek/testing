9b6bfa8aff29f2537280a29195cab50742efe1be
Manu Pareek <74110265+manu-pareek@users.noreply.github.com>
2021-11-11 13:06:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Foreground from "./Components/Foreground.js";
+
 
 console.log("inside index-foreground");
 
-ReactDOM.render(<Foreground />, document.getElementById("foreground"));
+ReactDOM.render(<h1 >Hello</h1>, document.getElementById("foreground"));
